b512b0e830e3f5f100028efda59aee8856d86f3d
Jon Maciel <joaomaciel.n@gmail.com>
2019-08-01 08:15:44 -0300

    Add Costumer public check existence

---
+++ app/graphql/types/public_customer_check_fields_enum.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Types
+  class PublicCustomerCheckFieldsEnum < Types::BaseEnum
+    value 'CELLPHONE_NUMBER', value: 'cellphone_number'
+    value 'EMAIL', value: 'email'
+    value 'CPF', value: 'cpf'
+  end
+end
--- app/graphql/types/public_query_type.rb
+++ app/graphql/types/public_query_type.rb
@@ -2,5 +2,20 @@
 
 module Types
   class PublicQueryType < Types::BaseObject
+    field :customer_check, Boolean, null: true do
+      argument :field, Types::PublicCustomerCheckFieldsEnum, required: true
+      argument :value, String, required: true
+    end
+
+    def customer_check(field:, value:)
+      case field
+      when 'cellphone_number'
+        Auth.where(cellphone_number: value)
+      when 'email'
+        Auth.where(email: value)
+      when 'cpf'
+        Customer.where(cpf: value)
+      end.any?
+    end
   end
 end
